9381cb612c169d8323b53b92323073123013dbf6
Mike Reed <mike.reed.vallejo@gmail.com>
2016-08-31 20:40:10 -0700

    setup TodoList

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,39 @@
 var React = require('react');
 
+var TodoList = require('TodoList');
+
 var TodoApp = React.createClass({
+  getInitialState: function () {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'wash the dog'
+        },
+        {
+          id: 2,
+          text: 'Feed the dog'
+        },
+        {
+          id: 3,
+          text: 'walk the cat'
+        },
+        {
+          id: 4,
+          text: 'de-flea the cat'
+        },
+        {
+          id: 5,
+          text: 'Eat the cat'
+        }
+      ]
+    }
+  },
   render: function () {
+    var {todos} = this.state;
     return (
       <div>
-        TodoApp.jsx
+        <TodoList todos={todos} />
       </div>
     )
   }
