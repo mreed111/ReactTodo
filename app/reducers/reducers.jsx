dfcefec373a3fa5bf8692fc8c41cdea0277935d0
Mike Reed <mike.reed.vallejo@gmail.com>
2016-09-19 16:08:35 -0700

    add showCompleted and searchText reducers

---
+++ app/reducers/reducers.jsx
@@ -0,0 +1,21 @@
+// searchText reducer
+//------------------------------------------------------------
+export var searchTextReducer = (state = '', action) => {
+  switch (action.type) {
+    case 'SET_SEARCH_TEXT':
+      return action.searchText;
+    default:
+      return state;
+  }
+};
+
+// showCompleted reducer
+//------------------------------------------------------------
+export var showCompletedReducer = (state = false, action) => {
+  switch (action.type) {
+    case 'TOGGLE_SHOW_COMPLETED':
+      return !state;
+    default:
+      return state;
+  }
+};
---
+++ app/tests/reducers/reducers.test.jsx
@@ -0,0 +1,31 @@
+var expect = require('expect');
+var df = require('deep-freeze-strict');
+
+var reducers = require('reducers');
+
+describe('Reducers', () => {
+  describe('searchTextReducer', () => {
+    it('should set searchText', () => {
+      var action = {
+        type: 'SET_SEARCH_TEXT',
+        searchText: 'look for this'
+      };
+      var res = reducers.searchTextReducer(df(''), df(action));
+
+      expect(res).toEqual(action.searchText);
+    });
+
+
+  });
+
+  describe('showCompletedReducer', () => {
+    it('should toggle showCompleted state.', () => {
+      var res = reducers.showCompletedReducer(df(false), df(action));
+      expect(res).toEqual(true);
+
+      res = reducers.showCompletedReducer(df(true), df(action));
+      expect(res).toEqual(false);
+    });
+  });
+
+});
